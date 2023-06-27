1a02549f7f617c7ae309bed3afbafd134c8e82cc
河口 欣仁 <kawakin0310@icloud.com>
2023-06-28 01:56:04 +0900

    ✨API から旅行時間を取得する関数を追加 `get_travel_time` 関数を持つ Python モジュールを追加 出発地と目的地の住所を入力として受け取りGoogle Directions API を呼び出し、推定旅行時間をテキスト形式で返す。 この関数は `requests` ライブラリを使用して API に HTTP GET リクエストを行う。

---
+++ calc_travel_time.py
@@ -0,0 +1,28 @@
+import requests
+
+def get_travel_time(origin, destination, api_key):
+    base_url = "https://maps.googleapis.com/maps/api/directions/json?"
+
+    # パラメータをURLに組み組み込む
+    complete_url = f"{base_url}origin={origin}&destination={destination}&key={api_key}"
+    
+    # APIを呼び出す
+    response = requests.get(complete_url)
+
+    #応答の確認
+    response.raise_for_status()
+
+    # 応答からJSONを取得
+    directions = response.json()
+
+    # ルートのリストから最初の提案を取得
+    route = directions['routes'][0]
+
+    # ルートからレッグのリストを取得し、最初のレッグ（通常は唯一のレッグ）を取得
+    leg = route['legs'][0]
+
+    # レッグから所要時間を取得
+    duration = leg['duration']
+
+    # 所要時間をリターン
+    return duration['text']
