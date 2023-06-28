d5c8d5bc087fe45b3e95fb005e64a677043757dd
河口 欣仁 <kawakin0310@icloud.com>
2023-06-29 00:23:37 +0900

    ✨住所と下校時刻によるグループ分け
    このコミットでは `BusRouting.py` の `groupStudentsByAttribute` メソッドを住所と下校時刻で生徒をグループ化するように変更しました。このメソッドは `address` と `dismissal_time` のタプルをキーとし、生徒のリストを値とする辞書を作成します。キーが既に存在する場合、その生徒は既存のリストに追加されます。最後に、辞書は下校時刻順、住所順にソートされたリストに変換されます。
    
    group_students` メソッドは削除され、グループ化された生徒を表示する新しいメソッド `printGroups` が追加されます。
    
    不要な空白行が削除され、メソッド `get_travel_time` の名前が `getTravelTime` に変更された。

--- BusRouting.py
+++ BusRouting.py
@@ -2,41 +2,55 @@ import requests
 from Bus import Bus
 from Student import Student
 
+
 class BusRouting:
     def __init__(self):
         self.students = []
         self.bus = []
-        
+
     def executeBusRouting(self, students, numberOfBuses):
         self.students = students
         for i in range(numberOfBuses):
             self.bus.append(Bus())
-            
+
         # 1. 生徒を住所と下校時間でグループ化する
-        groups = self.group_students(self.students)
-        print(groups)
-        
-    def group_students(self,students):
+        groups = self.groupStudentsByAttribute(self.students)
+        self.printGroups(groups)
+
+    def printGroups(self, groups):
+        print("========グループ化された生徒のリスト========")
+        for group in groups:
+            print(f"{group[0].address} {group[0].dismissal_time}:")
+            for student in group:
+                print(f"  {student.name}")
+
+    def groupStudentsByAttribute(self, students):
         # { (address, dismissal_time): [student1, student2, ...], ... }
-        groups = {}
+        groups_dict = {}
         for student in students:
-            key = (student.address, student.dismissal_time)
-            if key in groups:
-                groups[key].append(student)
+            key = (student.address, student.address, student.dismissal_time)
+            if key in groups_dict:
+                groups_dict[key].append(student)
             else:
-                groups[key] = [student]
-        return list(groups.values())
-        
-    def get_travel_time(origin, destination, api_key):
+                groups_dict[key] = [student]
+
+        # Convert dictionary to list and sort by dismissal time and school name
+        groups_list = list(groups_dict.values())
+        sorted_groups = sorted(groups_list, key=lambda group: (
+            group[0].dismissal_time, group[0].address))
+
+        return sorted_groups
+
+    def getTravelTime(origin, destination, api_key):
         base_url = "https://maps.googleapis.com/maps/api/directions/json?"
 
         # パラメータをURLに組み組み込む
         complete_url = f"{base_url}origin={origin}&destination={destination}&key={api_key}"
-        
+
         # APIを呼び出す
         response = requests.get(complete_url)
 
-        #応答の確認
+        # 応答の確認
         response.raise_for_status()
 
         # 応答からJSONを取得
