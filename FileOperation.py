51f9a9f47769bcfd8c9af01692c0ad2829efa120
河口 欣仁 <kawakin0310@icloud.com>
2023-06-28 11:23:43 +0900

    ♻️FileOperationモジュールのsetup_guiメソッドとrunメソッドのリファクタリング
    - setupGUIメソッドの不要なコードを削減
    - 命名規則に従うため、setup_guiメソッドをsetupGUIに改名
    - メソッドrunの名前をinputFileに変更し、明快さと一貫性を確保

--- FileOperation.py
+++ FileOperation.py
@@ -1,17 +1,16 @@
 import tkinter as tk  
 from tkinterdnd2 import DND_FILES, TkinterDnD  
-
 class FileOperation: 
     def __init__(self):  
         self.filepath = ''  
-        self.root = TkinterDnD.Tk()  
-        self.frame = tk.Frame(self.root, name='drag-drop-area', width=400, height=400)  # Initialize the frame in the constructor
 
     def drop(self,event): 
         self.filepath = event.data 
         self.root.quit()
 
-    def setup_gui(self):  # GUIの構成を行う新たなメソッドを作成
+    def setupGUI(self):  # GUIの構成を行う新たなメソッドを作成
+        self.root = TkinterDnD.Tk()  
+        self.frame = tk.Frame(self.root, name='drag-drop-area', width=400, height=400)  # Initialize the frame in the constructor
         self.frame.pack()  
         self.root.update() 
         self.root.dnd_accept = lambda x: x
@@ -19,7 +18,7 @@ class FileOperation:
         self.frame.drop_target_register(DND_FILES)  
         self.frame.dnd_bind('<<Drop>>', self.drop)
 
-    def run(self):  
+    def inputFile(self):  
         self.root.withdraw() 
         self.setup_gui()  # GUIの構成部分をここで呼び出す
         self.root.deiconify()  
