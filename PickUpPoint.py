c7916e7eea63f711c64327cfaa879e8f1782d56c
河口 欣仁 <kawakin0310@icloud.com>
2023-06-30 02:58:47 +0900

    Revert "✨♻️🚧"
    This reverts commit 4ff9c66fc952fa7e1a8fb9c43fba0ec37d12aee9.

--- PickUpPoint.py
+++
@@ -1,7 +0,0 @@
-class PickUpPoint:
-    _id = 0
-    def __init__(self,name,address):
-        self.id = PickUpPoint._id
-        self.name = name
-        self.address = address
-        PickUpPoint._id += 1
--- main.py
+++ main.py
@@ -1,31 +1,14 @@
-from ConsoleOperation import ConsoleOperation
 from FileOperation import FileOperation
 from BusRouting import BusRouting
 
+
 NUMBER_OF_BUSES = 3
 if __name__ == "__main__":
-    co = ConsoleOperation()
-    action = co.inputAction()
-
-    if 1 <= action <= 2:
-        # ファイル管理のためクラスインスタンスを作成
-        fo = FileOperation()
-
-        # ドラッグアンドドロップでファイルパスを取得
-        filePath = fo.inputFile()
-
-        # バスの経路を計算
-        if action == 1:
-            students = fo.setStudentData(filePath)
-
-            br = BusRouting()
-
-            br.executeBusRouting(students, NUMBER_OF_BUSES)
-
-        # ピックアップポイントの定義
-        elif action == 2:
-            registeredData = fo.registerPickUpPoint(filePath)
-
-            fo.printRegisteredData(registeredData)
-    else:
-        pass
+    fo = FileOperation()
+    dropped_file_path = fo.inputFile()
+    
+    students = fo.readCSV(dropped_file_path)
+    
+    br = BusRouting()
+    
+    br.executeBusRouting(students,NUMBER_OF_BUSES)
