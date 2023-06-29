e3fc4b15477760b869164510f3919539112e4e7e
河口 欣仁 <kawakin0310@icloud.com>
2023-06-30 08:49:22 +0900

    ♻️ConsoleOperationのinputActionメソッドをリファクタリング
    ConsoleOperation クラスの inputAction メソッドが getInput にリファクタリングされました。このメソッドは、オプションという追加のパラメータを受け取り、インデックスを入力し、利用可能なオプションと照らし合わせて検証し、確認後に選択されたインデックスを返す機能を提供します。また、より良いユーザーフィードバックを提供するためにコードが改善されました。

--- ConsoleOperation.py
+++ ConsoleOperation.py
@@ -1,10 +1,16 @@
 class ConsoleOperation:
-    def inputAction(self):
-        options = ["バス経路の出力", "ピックアップポイントの設定"]
+    def getInput(self,options):
         while True:
             for i in range(len(options)):
                 print(str(i) + "." + options[i])
-            action = int(input(">>"))
-            if 0 <= action <= len(options) - 1:
-                break
-        return action
+            index = int(input(">>"))
+            if 0 <= index <= len(options) - 1:
+                while True:
+                    print("選択されたのは" + options[index] + "です。")
+                    confirm = input("この操作でよろしいですか？ (yes/no): ").lower()
+                    if confirm == "yes" or confirm == "no":
+                        if confirm == "yes":
+                            return index
+                        else:
+                            print("再度入力してください。")
+                            break
