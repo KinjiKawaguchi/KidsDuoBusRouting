2385a33912d0be4ed362d44bd6e64cbc06b864a5
河口 欣仁 <kawakin0310@icloud.com>
2023-06-30 08:46:53 +0900

    ✨データベースに対してCRUD操作を行えるようにUIや機能を実装 feat(DatabaseManager)：クラスメソッドのリファクタリング
    - DatabaseManagerクラスのメソッド名を更新し、一貫した命名規則に従うようにした。
    - テーブルが空かどうかのチェック、場所の削除、場所の更新、データベースからすべての場所を取得するための新しいメソッドを追加しました。
    - 不要な print 文を削除しました。
    
    feat(FileOperation)：ピックアップポイントデータの管理
    
    - データベースが空の場合にピックアップポイントの入力を促す機能を追加。
    - ピックアップポイントの登録、全ピックアップポイントの表示、ピックアップポイントの更新、ピックアップポイントの削除のメソッドを実装。
    
    feat(main)：リファクタリングとオプションの追加
    
    - ユーザー入力にConsoleOperationクラスを使用するようにmain.pyファイルをリファクタリングしました。
    - バスルートの計算とピックアップポイントデータの管理オプションを追加。
    - ユーザー入力に基づいてバスの経路計算を実行し、ピックアップポイントデータを管理する機能を実装。

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -6,19 +6,36 @@ class DatabaseManager:
         self.cursor = self.conn.cursor()
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS Places (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
                 name TEXT UNIQUE,
                 address TEXT UNIQUE
             );
         ''')
         self.conn.commit()
 
-    def check_place_exists(self, name, address):
+    def checkPlaceExistIs(self, name, address):
         self.cursor.execute("SELECT * FROM Places WHERE name = ? OR address = ?", (name, address))
         return self.cursor.fetchone() is not None
 
-    def add_place(self, name, address):
+    def checkTableEmptyIs(self):
+        self.cursor.execute("SELECT COUNT(*) FROM Places")
+        return self.cursor.fetchone()[0] == 0
+
+    def addPlace(self, name, address):
         try:
             self.cursor.execute("INSERT INTO Places (name, address) VALUES (?, ?)", (name, address))
             self.conn.commit()
         except sqlite3.IntegrityError:
             print(f"Error: '{name}' or '{address}' already exists in the database.")
+
+    def deletePlace(self, id):
+        self.cursor.execute("DELETE FROM Places WHERE id = ?", (id,))
+        self.conn.commit()
+
+    def updatePlace(self, id, new_name, new_address):
+        self.cursor.execute("UPDATE Places SET name = ?, address = ? WHERE id = ?", (new_name, new_address, id))
+        self.conn.commit()
+
+    def getAllplaces(self):
+        self.cursor.execute("SELECT * FROM Places")
+        return self.cursor.fetchall()
--- main.py
+++ main.py
@@ -5,27 +5,43 @@ from BusRouting import BusRouting
 NUMBER_OF_BUSES = 3
 if __name__ == "__main__":
     co = ConsoleOperation()
-    action = co.inputAction()
+    options = ["バスの経路を計算", "ピックアップポイントのデータ管理"]
+    action = co.getInput(options)
 
-    if 1 <= action <= 2:
-        # ファイル管理のためクラスインスタンスを作成
-        fo = FileOperation()
+    # ファイル管理のためクラスインスタンスを作成
+    fo = FileOperation()
+
+    if action == 0:
+        # バスの経路を計算
 
         # ドラッグアンドドロップでファイルパスを取得
         filePath = fo.inputFile()
+        students = fo.setStudentData(filePath)
 
-        # バスの経路を計算
-        if action == 1:
-            students = fo.setStudentData(filePath)
+        br = BusRouting()
+        br.executeBusRouting(students, NUMBER_OF_BUSES)
 
-            br = BusRouting()
+    elif action == 1:
+        # ピックアップポイントのデータ管理
+        options = ["登録", "表示", "更新", "削除"]
+        crud_action = co.getInput(options)
 
-            br.executeBusRouting(students, NUMBER_OF_BUSES)
+        if crud_action == 0:  # Create
+            # ドラッグアンドドロップでファイルパスを取得
+            filePath = fo.inputFile()
+            fo.registerPickUpPoint(filePath)
 
-        # ピックアップポイントの定義
-        elif action == 2:
-            registeredData = fo.registerPickUpPoint(filePath)
+        elif crud_action == 1:  # Read
+            fo.printPickupPoint()
 
-            fo.printRegisteredData(registeredData)
+        elif crud_action == 2:  # Update
+            id = co.getSelectedDataIndex()
+            new_name = co.inputNewName()
+            new_address = co.inputNewAddress()
+            fo.update_pickup_point(id, new_name, new_address)
+
+        elif crud_action == 3:  # Delete
+            id = co.getSelectedDataIndex()
+            fo.delete_pickup_point(id)
     else:
         pass
