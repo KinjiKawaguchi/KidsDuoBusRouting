4f00b141516c5e114a4e12d3aa14dde085623cf7
KinjiKawaguchi <kawakin0310@icloud.com>
2023-07-26 18:08:30 +0900

    ♻️データベースのリファクタリング
    - 場所とルートセグメントのデータベーステーブル名をリファクタリングしました。
    - テーブルのカラム名と制約を更新し、わかりやすさと読みやすさを改善しました。
    - テーブル名とカラム名の変更を反映するため、メソッド名とクエリを更新しました。
    - 重複する delete_route_segment() メソッドを削除しました。
    - ファイル末尾の空白を削除しました。

--- src/db/DatabaseManager.py
+++ src/db/DatabaseManager.py
@@ -6,44 +6,45 @@ class DatabaseManager:
         self.conn = sqlite3.connect(db_name)
         self.cursor = self.conn.cursor()
         self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS Places (
+            CREATE TABLE IF NOT EXISTS places (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT UNIQUE,
-                address TEXT UNIQUE
+                name TEXT,
+                address TEXT,
+                UNIQUE (name, address) 
             );
         ''')
 
         self.cursor.execute('''
-            CREATE TABLE IF NOT EXISTS BusRouteSegment (
+            CREATE TABLE IF NOT EXISTS route_segments (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                FromId INTEGER,
-                ToId INTEGER,
-                Time REAL,
-                Distance REAL,
-                FOREIGN KEY (FromId) REFERENCES Places(id),
-                FOREIGN KEY (ToId) REFERENCES Places(id)
+                origin_id INTEGER,
+                destination_id INTEGER,
+                duration REAL,  -- 単位：秒
+                distance REAL,  -- 単位：メートル
+                FOREIGN KEY (origin_id) REFERENCES places(id),
+                FOREIGN KEY (destination_id) REFERENCES places(id)
             );
         ''')
 
         self.conn.commit()
 
     def is_pickup_point_exits(self, id):
-        self.cursor.execute("SELECT * FROM Places WHERE id = id", (id,))
+        self.cursor.execute("SELECT * FROM places WHERE id = id", (id,))
         return self.cursor.fetchone() is not None
 
     def is_table_empty(self):
-        self.cursor.execute("SELECT COUNT(*) FROM Places")
+        self.cursor.execute("SELECT COUNT(*) FROM places")
         return self.cursor.fetchone()[0] == 0
 
     def add_pickup_point(self, name, address):
         try:
             self.cursor.execute(
-                "INSERT INTO Places (name, address) VALUES (?, ?)", (name, address))
+                "INSERT INTO places (name, address) VALUES (?, ?)", (name, address))
             self.conn.commit()
 
             # Return the added data
             self.cursor.execute(
-                "SELECT * FROM Places WHERE name = ? AND address = ?", (name, address))
+                "SELECT * FROM places WHERE name = ? AND address = ?", (name, address))
             return self.cursor.fetchone()
         except sqlite3.IntegrityError:
             print(
@@ -51,66 +52,67 @@ class DatabaseManager:
             return False
 
     def get_pickup_point(self, id):
-        self.cursor.execute("SELECT * FROM Places WHERE id = ?", (id,))
+        self.cursor.execute("SELECT * FROM places WHERE id = ?", (id,))
         return self.cursor.fetchone()
 
     def get_all_pickup_points(self):
-        self.cursor.execute("SELECT * FROM Places")
+        self.cursor.execute("SELECT * FROM places")
         return self.cursor.fetchall()
 
     def update_pickup_point(self, id, new_name, new_address):
         self.cursor.execute(
-            "UPDATE Places SET name = ?, address = ? WHERE id = ?", (new_name, new_address, id))
+            "UPDATE places SET name = ?, address = ? WHERE id = ?", (new_name, new_address, id))
         self.conn.commit()
         # Return the updated data
-        self.cursor.execute("SELECT * FROM Places WHERE id = ?", (id,))
+        self.cursor.execute("SELECT * FROM places WHERE id = ?", (id,))
         return self.cursor.fetchone()
 
     def delete_pickup_point(self, id):
         # Fetch the data before deletion
-        self.cursor.execute("SELECT * FROM Places WHERE id = ?", (id,))
+        self.cursor.execute("SELECT * FROM places WHERE id = ?", (id,))
         data = self.cursor.fetchone()
-        self.cursor.execute("DELETE FROM Places WHERE id = ?", (id,))
+        self.cursor.execute("DELETE FROM places WHERE id = ?", (id,))
         self.conn.commit()
         # Return the deleted data
         return data
 
     def count_pickup_point(self):
-        self.cursor.execute("SELECT COUNT(*) FROM Places")
+        self.cursor.execute("SELECT COUNT(*) FROM places")
         return self.cursor.fetchone()[0]
+    
 # ------------------------(RouteSegmentTable)-------------------------------------------
 
     def is_route_segment_exits(self, origin_id, destination_id):
         pass
 
     def add_route_segment(self, added_id, comparing_id, duration, distance):
-        self.cursor.execute("INSERT INTO BusRouteSegment (FromId, ToId, Time, Distance) VALUES (?, ?, ?, ?)",
+        self.cursor.execute("INSERT INTO route_segments (origin_id, destination_id, duration, distance) VALUES (?, ?, ?, ?)",
                             (added_id, comparing_id, duration, distance))
         self.conn.commit()
         return self.cursor.fetchone()
 
     def get_route_segment(self, origin_id, destination_id):
         self.cursor.execute(
-            "SELECT * FROM BusRouteSegment WHERE FromId = ? AND ToId = ?", (origin_id, destination_id))
+            "SELECT * FROM route_segment WHERE origin_id = ? AND destination_id = ?", (origin_id, destination_id))
         return self.cursor.fetchone()
     
     def get_all_route_segments(self):
-        self.cursor.execute("SELECT * FROM BusRouteSegment")
+        self.cursor.execute("SELECT * FROM route_segment")
         return self.cursor.fetchall()
 
     def update_route_segment(self, origin_id, destination_id, new_origin_id, new_destination_id, new_duration, new_distance):
-        self.cursor.execute("UPDATE BusRouteSegment SET FromId = ?, ToId = ?, Time = ?, Distance = ? WHERE FromId = ? AND ToId = ?",
+        self.cursor.execute("UPDATE route_segment SET origin_id = ?, destination_id = ?, duration = ?, distance = ? WHERE origin_id = ? AND destination_id = ?",
                             (new_origin_id, new_destination_id, new_duration, new_distance, origin_id, destination_id))
         updated_data = self.cursor.fetchone()
         return updated_data
 
     def delete_route_segment(self, id):
-        self.cursor.execute("DELETE FROM BusRouteSegment WHERE id = ?", (id,))
+        self.cursor.execute("DELETE FROM route_segment WHERE id = ?", (id,))
         deleted_data = self.cursor.fetchone()
         return deleted_data
     
     def delete_route_segment(self, origin_id, destination_id):
-        self.cursor.execute("DELETE FROM BusRouteSegment WHERE FromId = ? AND ToId = ?", (origin_id, destination_id))
+        self.cursor.execute("DELETE FROM route_segment WHERE origin_id = ? AND destination_id = ?", (origin_id, destination_id))
         deleted_data = self.cursor.fetchone()
         return deleted_data
     
